72104e1d47245a9f20753a5871be285d79a17de2
Nicolás Jiménez <estnicjimsae@hotmail.com>
2020-02-27 21:36:37 -0500

    Nicolas.J-Implementación inicial de consumo de colas SQS.

--- testerxp/testerxp-api/src/index.js
+++ testerxp/testerxp-api/src/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+// Load the AWS SDK for Node.js
+const AWS = require('aws-sdk');
+// Set the region we will be using
+AWS.config.update({region: 'us-east-1'});
 
 //Initializations
 const app = express();
@@ -25,4 +29,25 @@ app.use('/historico-prueba', require('./routes/historico-prueba'));
 //Starting the server
 app.listen(app.get('port'), () => {
   console.log('Server on port', app.get('port'));
+});
+
+// Create SQS service client
+const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+// Replace with your account id and the queue name you setup
+const accountId = '1234567';
+const queueName = 'test';
+// Setup the sendMessage parameter object
+const params = {
+  MessageBody: JSON.stringify({
+    order_id: 1234,
+    date: (new Date()).toISOString()
+  }),
+  QueueUrl: `https://sqs.us-east-1.amazonaws.com/${accountId}/${queueName}`
+};
+sqs.sendMessage(params, (err, data) => {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Successfully added message", data.MessageId);
+  }
 });
