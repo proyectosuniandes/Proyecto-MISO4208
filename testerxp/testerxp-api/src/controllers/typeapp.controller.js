2dd5034404dfa7472c06a324314b9cb4d1e9e9b5
ha.ortiz11@uniandes.edu.co <Leon@1480>
2020-03-02 11:25:54 -0500

    CRUD de aplicaciones

--- testerxp/testerxp-api/src/controllers/typeapp.controller.js
+++ testerxp/testerxp-api/src/controllers/typeapp.controller.js
@@ -1,19 +1,16 @@
 const TipoApp = require('../models/TipoApp');
 
-
 exports.findAll = async (req, res) => {
     console.log("findAll");
     try {
         const {range, sort, filter} = req.query;
         const [from, to] = range ? JSON.parse(range) : [0, 100];
-        //const parsedFilter = filter ? parseFilter(filter) : {}
-        console.log(sort);
-        console.log(filter);
+        const parsedFilter = filter ? parseFilter(filter) : {}
         const {count, rows} = await TipoApp.findAndCountAll({
             offset: from,
             limit: to - from + 1,
             order: [sort ? JSON.parse(sort) : ['id', 'ASC']],
-            where: {},
+            where: parsedFilter,
             raw: true,
         });
         res.set('Content-Range', `${from}-${from + rows.length}/${count}`);
@@ -25,4 +22,29 @@ exports.findAll = async (req, res) => {
         res.status(500).json({ message: "couldn't retrieve tipoPruebas" });
     }
 
+};
+
+export const parseFilter = (filter: string) => {
+    const filters = JSON.parse(filter)
+    return Object.keys(filters)
+        .map(key => {
+            if (
+                typeof filters[key] === 'string' &&
+                filters[key].indexOf('%') !== -1
+            ) {
+                return {
+                    [key]: {[Op.like]: filters[key]},
+                }
+            }
+            return {
+                [key]: filters[key],
+            }
+        })
+        .reduce(
+            (whereAttributes, whereAttribute) => ({
+                ...whereAttributes,
+                ...whereAttribute,
+            }),
+            {}
+        )
 };
