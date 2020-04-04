6a3cea61780773ea529532e208da2f567d8725fe
Daniela <laura_chacon-93@hotmail.com>
2020-04-03 21:55:23 -0500

    Arreglo mayúsculas

--- testerxp/testerxp-api/src/routes/execution.routes.js
+++
@@ -1,6 +0,0 @@
-module.exports = (app) => {
-  const Executions = require('../controllers/execution.controller.js');
-
-  //Retrieve all Executions
-  app.get('/executions', Executions.findAll);
-};
